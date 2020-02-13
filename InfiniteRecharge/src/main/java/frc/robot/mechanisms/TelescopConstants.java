5d7c04c0a5d1351b99ccde70cc06f78e0fd8aa81
5010 TigerDynasty10 <roboticsteam.fhs10@gmail.com>
2020-02-13 17:47:52 -0500

    New Climb
    New telescopic climb complete with mechanisms, subsystem, and commands.  Will need to talk about the logistics of it such as controls and full functionality/

---
+++ InfiniteRecharge/src/main/java/frc/robot/mechanisms/TelescopConstants.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.mechanisms;
+
+/**
+ * Add your docs here.
+ */
+public class TelescopConstants {
+    //Values left null must be decided by electric.
+    public static int arm1Port;
+    public static int arm2Port;
+    public static int winch1Port = 6;
+    public static int winch2Port = 7;
+
+    public static double winchSpeed = 0.5;
+    public static double armSpeed = 0.5;
+
+    public static int encoderPort1;
+    public static int encoderPort2;
+    public static int encoderPort3;
+    public static int encoderPort4;
+
+    public static int arm1Axis;
+    public static int arm2Axis;
+    public static int winch1Axis;
+    public static int winch21Axis;
+    
+
+}
