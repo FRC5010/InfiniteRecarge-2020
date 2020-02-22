f57d14b538d1d02047989065a5d4fb61a0851db3
5010 TigerDynasty10 <roboticsteam.fhs10@gmail.com>
2020-02-22 13:50:53 -0500

    fixed winch issues

--- InfiniteRecharge/src/main/java/frc/robot/commands/ClimbArm1.java
+++ InfiniteRecharge/src/main/java/frc/robot/commands/ClimbArm1.java
@@ -30,14 +30,12 @@ public class ClimbArm1 extends CommandBase {
   @Override
   public void execute() {
     subsystem.spinArmMotor1();
-    subsystem.spinArmMotor2();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     subsystem.stopArmMotor1();
-    subsystem.stopArmMotor2();
   }
 
   // Returns true when the command should end.
