266e01d822a0895a6f9a19e38753d3f1ca705a79
5010TigerDynasty <roboticsteam.fhs@gmail.com>
2020-07-31 18:56:21 -0400

    Update ShooterConstants.java
    changed shooter constants

--- InfiniteRecharge/src/main/java/frc/robot/mechanisms/ShooterConstants.java
+++ InfiniteRecharge/src/main/java/frc/robot/mechanisms/ShooterConstants.java
@@ -17,8 +17,16 @@ public class ShooterConstants {
     public static final double ballRadius = 0.09; // m
     public static final double innerPortRadius = 0.16; // m
     public static final double innerPortHeight = 2.500; // meters, measured from floor to center
-    public static double baseSpeed = 2510;
-    public static double distanceToRPM = 5.7;
+    public static double baseSpeed = 4267;
+    //4500 scores at 7 ft with distancetoRPM at 0
+    //4750 scores at 10 ft (about 100 per feet until we hit 10 feet)
+    //4830 scores at 12 ft
+    //4850 scores better at 14 ft
+    //4870 scores at 16 ft
+    //4890 scores at 17 ft
+    //4950 scores at 18 ft
+    //Around 5180 at 19 ft
+    public static double distanceToRPM = 3.46;
     public static double getBaseSpeed() { return baseSpeed; }
     public static double getDistanceToRPM() { return distanceToRPM; }
     static {
